390766332347308f4b56f081dafc59127f9090dc
jhillgithub <jeremy.hill.email@gmail.com>
2017-06-28 11:07:55 -0500

    Create aws_setup.sh

---
+++ aws_setup.sh
@@ -0,0 +1,17 @@
+# SSH into the server
+ssh -i /path/to/*.pem ec2-user@$APP_IP_ADDRESS
+
+# As root, run the following:
+
+# First, install docker
+sudo yum update -y
+sudo yum install -y docker
+
+# Run docker
+sudo service docker start
+
+# Add ec2-user to docker group so you can run docker without sudo
+sudo usermod -a -G docker ec2-user
+
+# Test Docker
+docker info
